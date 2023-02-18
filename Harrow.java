e619d4f048f84d6c34cb9b5f36ee8f648d234582
divya vandra <divyavandra@gmail.com>
2023-02-18 23:49:19 +0000

    all about london

---
+++ Harrow.java
@@ -0,0 +1,15 @@
+package Towns;
+
+//I am going to write here all about Harrow Town Center
+
+
+public class Harrow {
+    public static void main(String[] args) {
+        System.out.println("Harrow town center is in London");
+        System.out.println("It is one of the busy town");
+        System.out.println("There are so many shops and Banks");
+/* There are so many things to do in Harrow Town Center
+   It is big and beautiful.
+ */
+    }
+}
---
+++ Stanmore.java
@@ -0,0 +1,12 @@
+package Towns;
+
+//There are so many towns in London
+// I like Stanmore the most
+public class Stanmore {
+    public static void main(String[] args) {
+        System.out.println("I like Stanmore");
+        System.out.println("So many nice schools are available in Stanmore");
+
+
+    }
+}
